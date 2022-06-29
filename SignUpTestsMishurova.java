539cb28641615cbddbc11b64be8c60c6518c780f
Radoslava486 <108293735+Radoslava486@users.noreply.github.com>
2022-06-30 01:15:29 +0300

    Update SignUpTestsMishurova.java

--- SignUpTestsMishurova.java
+++ SignUpTestsMishurova.java
@@ -1,27 +1,27 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SignUpTestsMishurova {
-    private WebDriverManager driver;
+    private WebDriver driver;
 
 
-   
-    @Before
+
+    @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
     }
 
-    @After
+    @AfterClass
     public void close() {
         driver.quit();
     }
@@ -106,16 +106,19 @@ public class SignUpTestsMishurova {
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
         continueButton.click();
-        driver.findElement(By.name("first_name")).sendKeys("");
-        driver.findElement(By.name("email")).sendKeys("");
-        driver.findElement(By.name("password1")).sendKeys("");
-        driver.findElement(By.name("password2")).sendKeys("");
+        Assert.assertFalse(driver.findElement(By.name("zip_code")).isDisplayed());
+
+        driver.findElement(By.name("first_name")).sendKeys("Rada");
+        driver.findElement(By.name("email")).sendKeys("mishurova486@gmail.com");
+        driver.findElement(By.name("password1")).sendKeys("123456");
+        driver.findElement(By.name("password2")).sendKeys("123456");
+
         WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
         registerButton.click();
-        driver.findElement(By.name("keyword")).sendKeys("Leo Tolstoy");
+        driver.findElement(By.cssSelector("[type ='text']")).sendKeys("War and Peace");
         WebElement searchButton = driver.findElement(By.cssSelector("[value='Search']"));
         searchButton.click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("War and Peace")).isDisplayed(), " Relevant books should be displayed " );
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()=‘War and Peace’]")).isDisplayed(), " Relevant books should be displayed " );
 
     }
 
@@ -124,17 +127,16 @@ public class SignUpTestsMishurova {
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
         continueButton.click();
-        driver.findElement(By.name("first_name")).sendKeys("");
-        driver.findElement(By.name("email")).sendKeys("");
-        driver.findElement(By.name("password1")).sendKeys("");
-        driver.findElement(By.name("password2")).sendKeys("");
+        Assert.assertFalse(driver.findElement(By.name("zip_code")).isDisplayed());
+
+        driver.findElement(By.name("first_name")).sendKeys("Rada");
+        driver.findElement(By.name("email")).sendKeys("mishurova486@gmail.com");
+        driver.findElement(By.name("password1")).sendKeys("123456");
+        driver.findElement(By.name("password2")).sendKeys("123456");
         WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
         registerButton.click();
         driver.findElement(By.name("keyword")).sendKeys("Leo Tolstoy");
         WebElement searchButton = driver.findElement(By.cssSelector("[value='Search']"));
         searchButton.click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("Leo Tolstoy")).isDisplayed(), " Relevant books should be displayed " );
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()=‘Leo Tolstoy’]")).isDisplayed(), " Relevant books should be displayed " );
     }
-
-
-}
